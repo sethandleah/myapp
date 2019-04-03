eccdd15cd29a5288ea0b5a77d68de10cd2029efa
sibabale <sibabale@MACBOOKs-MacBook-Pro.local>
2019-04-04 01:36:43 +0200

    init commit

---
+++ src/Accordion.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+function Accordion({ children }) {
+
+ return (
+
+   <div className="accordion">
+     { children }
+   </div>
+
+ );
+
+};
+
+export default Accordion;
